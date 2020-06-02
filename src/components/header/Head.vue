c5779a4e1c7603de8622d779158936087a07b61d
snpdcfg <2567570935@qq.com>
2020-06-02 12:25:41 +0800

    test

---
+++ src/components/header/Head.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="head">head</div>
+</template>
+
+<script>
+export default {
+  name: 'Head'
+}
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'router'
+
+Vue.use(Router)
+
+export default new Router({
+  modu: 'history',
+  routers: [
+    {
+      path: '/',
+      name: 'root',
+
+    }
+  ]
+})
